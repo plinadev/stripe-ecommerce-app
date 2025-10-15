712d1b7cc019ccf2a9f43fbe697d0887ed08c4b6
plinadev <pavlenkopolina415@gmail.com>
2025-10-15 11:37:50 +0300

    Initial project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>eCommerce stripe app</div>;
+}
+
+export default App;
---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || "3001";
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+app.listen(PORT, () => {
+  console.log(`Listening to http://localhost:${PORT}`);
+});
